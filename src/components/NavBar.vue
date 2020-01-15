cb7f6de24cb67c2fbd900ba5a3de1598278afb20
ericwm76 <eric.meldrum@gmail.com>
2020-01-15 08:38:00 -0700

    Style navbar Search input and Submit button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="nav-bar">
-    <h1>FotoFindur!</h1>
-    <h2>Find ur fotos!</h2>
+    <h1 id="header">FotoFindur!</h1>
+    <h2 id="subheader">Find ur fotos!</h2>
     <form>
       <input v-model="searchTerm" type="text" placeholder="Search for photos here" />
       <button @click="handleSubmit">Submit</button>
@@ -28,5 +28,38 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  background-color: rgb(255, 234, 139);
+  margin: 0;
+}
 
+#header {
+  font-family: 'Pacifico', cursive;
+  margin: 0;
+}
+
+#subheader {
+  font-family: 'Quicksand', sans-serif;
+  margin: 0;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  display: inline-block;
+  height: 50px;
+  margin: 0 20px;
+  width: 25%;
+}
+
+button {
+  display: inline-block;
+  height: 50px;
+  margin: 0;
+  width: 15%;
+}
 </style>
