36a7a8015a2acdca1f493e9300dcb1bdad4ad175
ericwm76 <eric.meldrum@gmail.com>
2020-01-15 09:31:36 -0700

    Style main container so images enlarge on hover without moving other rows

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <template>
   <main id="main-container">
     <div v-for="photo in photos" :key="photo.id">
-      <img v-bind:src="photo.urls.small" v-bind:alt="photo.alt_description" />
+      <div id="image-container">
+        <img v-bind:src="photo.urls.small" v-bind:alt="photo.alt_description" />
+      </div>
     </div>
+    <!-- <button v-if="photos.length" id="load-button">Load more</button> -->
   </main>
 </template>
 
@@ -20,10 +23,15 @@ main {
   display: grid;
   grid-gap: 10px;
   grid-template-columns: 40% 40%;
+  grid-template-rows: repeat(auto, 280px);
   padding: 10px;
   margin: 0 10%;
   width: 100%;
 }
+#image-container {
+  height: 280px;
+}
+
 img {
   align-items: center;
   border: 1px solid lightgrey;
@@ -31,5 +39,16 @@ img {
   height: 270px;
   width: 405px;
 }
+img:hover {
+  height: 275px;
+  width: 410px;
+  transition: .2s;
+}
+
+/* #load-button {
+  background-color: white;
+  color: steelblue;
+  margin: auto;
+} */
 
 </style>
