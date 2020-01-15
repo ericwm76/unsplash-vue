d312270dc360a302bc103616f031f73345f01b44
ericwm76 <eric.meldrum@gmail.com>
2020-01-15 05:09:12 -0700

    Create NavBar component and import it to App

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav id="nav-bar">
+    <h1>FotoFindur!</h1>
+    <h2>Find ur fotos!</h2>
+    <form>
+      <input type="text" placeholder="Search for photos here" />
+      <button></button>
+    </form>
+  </nav>  
+</template>
+
+<script>
+export default {
+  name: 'nav-bar'
+}
+</script>
+
+<style scoped>
+
+</style>
