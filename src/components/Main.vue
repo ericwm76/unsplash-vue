2cfe2e296ca0e106717d8fcfee160b30e9eaac65
ericwm76 <eric.meldrum@gmail.com>
2020-01-15 07:58:15 -0700

    Cause fetched images to render

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <main id="main">
     <div v-for="photo in photos" :key="photo.id">
-
+      <img v-bind:src="photo.urls.full" v-bind:alt="photo.alt_description" />
     </div>
   </main>
 </template>
@@ -16,5 +16,9 @@ export default {
 </script>
 
 <style scoped>
+img {
+  height: 360px;
+  width: 480px;
+}
 
 </style>
