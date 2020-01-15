08bd110bc8d6142bfd706f9c1682813ae8125e1b
ericwm76 <eric.meldrum@gmail.com>
2020-01-15 05:24:39 -0700

    Create Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main id="main">
+    <div v-for="photo in photos" :key="photo.id">
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  props: {
+    photos: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
